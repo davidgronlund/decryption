0140ef468be047fa67e04b05b29b7c8bc36d28fd
David Grönlund <david.gronlund@sigma.se>
2018-10-14 21:13:27 +0200

    Added crypto decryption

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace aggregate_practice
+{
+    class Program
+    {
+        static readonly string alphabet = "abcdefghijklmnopqrstuvwxyzåäö";
+        static string crypto = "Pwzqemuözrözvykvvgt".ToLower();
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, alphabet.Length - 2)
+                .Select(x => x);
+            numbers.ToList().ForEach(displaceBy =>
+            {
+                var decrypted = crypto
+                    .ToCharArray()
+                    .Select(r =>
+                    {
+                        var index = (alphabet.IndexOf(r) - displaceBy) < 0 && crypto.Length >= displaceBy
+                            ? alphabet.Length + (alphabet.IndexOf(r) - displaceBy)
+                            : alphabet.IndexOf(r) - displaceBy;
+                        return alphabet.Substring((index), 1);
+                    })
+                    .Aggregate((current, next) => current.ToString() + next.ToString())
+                    .Replace("x", " ");
+                Console.WriteLine(decrypted);
+            });
+        }
+    }
+}
